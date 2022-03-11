061466df743fcd75cc5129f9b702e7110220878f
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2022-03-12 01:20:39 +0700

    Fetch pokemon data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [pokemons, setPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(`https://pokeapi.co/api/v2/pokemon`);
+
+  function createPokemonObject(results) {
+    results.forEach((pokemon) => {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        .then((res) => {
+          const data = res.data;
+          setPokemons((prev) => [...prev, data]);
+        });
+    });
+  }
+
+  function getAllPokemons() {
+    axios
+      .get(loadMore)
+      .then((res) => {
+        const data = res.data;
+        setLoadMore(data.next);
+        createPokemonObject(data.results);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Pokedex</h1>
+      <button onClick={getAllPokemons}>Fetch more Pokemons!!!</button>
+    </div>
+  );
 }
 
 export default App;
